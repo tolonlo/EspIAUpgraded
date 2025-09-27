2512501b7d8141ef3d1fcc6884e8f60fc7d31ff7
Figs0203 <figuesicsi@gmail.com>
2025-09-27 09:42:33 -0500

    Modelos, admin y migrations listos

--- Project/web/admin.py
+++ Project/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "created_at", "updated_at")
+    list_filter = ("status",)
+    search_fields = ("title", "description")
